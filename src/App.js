16ef4205e8779f369eb4eafd35ea46a19272a6ea
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-05-03 12:24:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { HomeView } from './Routes/Home/HomeView';
 import { CalculateView } from './Routes/Calculate/CalculateView';
-import  RecipesView  from './Components/Recipes/RecipesView';
+import RecipesView from './Components/Recipes/RecipesView';
+import { NavbarComponent } from './Components/Navbar/NavbarComponent';
 
 
 function App() {
   return (
     <Router>
-    {/* <NavbarComponent /> */}
-    <Routes>
-      <Route path="/" element={<HomeView />} /> 
-      <Route path="/calculate" element={<CalculateView />} />
-      <Route path="/recipes" element={<RecipesView />} />
-    </Routes>
-   
-  </Router>
+        <NavbarComponent />
+      <Routes>
+        <Route path="/" element={<HomeView />} />
+        <Route path="/calculate" element={<CalculateView />} />
+        <Route path="/recipes" element={<RecipesView />} />
+      </Routes>
+
+    </Router>
   );
 }
 
