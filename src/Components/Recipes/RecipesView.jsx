60a335d82c97cd7f82f1582325a1bce57c7879c5
Georgi Tsankov <145467172+GeorgiGeorgievTsankov@users.noreply.github.com>
2024-12-19 14:45:05 +0200

    Update RecipesView.jsx

--- src/Components/Recipes/RecipesView.jsx
+++ src/Components/Recipes/RecipesView.jsx
@@ -9,8 +9,6 @@ const RecipesView = () => {
 
     return (
         <div className="recipes-view animation">
-            <div className='uchebni-celi animation'><h3>If the recipes are not loading, the reason may be that the reached API requests have reached the maximum limit for the day from the free resource I use for educational purposes.</h3></div>
-            <h1>Recipes</h1>
             <APIRequestComponent/>
         </div>
     );
